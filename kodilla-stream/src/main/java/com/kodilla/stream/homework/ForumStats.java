9052a557570261c09647cf4b0fd4e2b10a0417b4
Michal Nowak <mnpositiveengineer@gmail.com>
2020-03-15 13:19:56 +0000

    ExceptionAddedToForumStats

--- kodilla-stream/src/main/java/com/kodilla/stream/homework/ForumStats.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/homework/ForumStats.java
@@ -1,25 +1,39 @@
 package com.kodilla.stream.homework;
 
+import com.kodilla.stream.Users;
 import com.kodilla.stream.UsersRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
 
 public class ForumStats {
     public static void main(String[] args) {
-        double avg1 = UsersRepository.getUsersList()
-                .stream()
-                .filter(u -> u.getAge()>=40)
-                .map(u -> u.getNumberOfPosts())
-                .mapToInt(u -> u)
-                .average()
-                .getAsDouble();
-        System.out.println(avg1);
+        List<Users> usersList = UsersRepository.getUsersList();
+        try {
+            double avg1 = usersList
+                    .stream()
+                    .filter(u -> u.getAge() >= 40)
+                    .map(u -> u.getNumberOfPosts())
+                    .mapToInt(u -> u)
+                    .average()
+                    .getAsDouble();
+            System.out.println(avg1);
+        } catch (NoSuchElementException e){
+            System.out.println("There are no people over age of 40");
+        }
 
-        double avg2 = UsersRepository.getUsersList()
-                .stream()
-                .filter(u -> u.getAge()<40)
-                .map(u -> u.getNumberOfPosts())
-                .mapToInt(u -> u)
-                .average()
-                .getAsDouble();
-        System.out.println(avg2);
+        try {
+            double avg2 = usersList
+                    .stream()
+                    .filter(u -> u.getAge() < 40)
+                    .map(u -> u.getNumberOfPosts())
+                    .mapToInt(u -> u)
+                    .average()
+                    .getAsDouble();
+            System.out.println(avg2);
+        } catch (NoSuchElementException e) {
+            System.out.println("There are no people under age of 40");
+        }
     }
 }
