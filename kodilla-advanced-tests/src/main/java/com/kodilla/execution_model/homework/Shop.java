eb4536d3e2a9ca33bd3e85a4a2363c8559cc73f7
Michal Nowak <mnpositiveengineer@gmail.com>
2020-03-22 11:44:11 +0000

    ShopTestSuite - refinment

--- kodilla-advanced-tests/src/main/java/com/kodilla/execution_model/homework/Shop.java
+++ kodilla-advanced-tests/src/main/java/com/kodilla/execution_model/homework/Shop.java
@@ -11,17 +11,36 @@ public class Shop {
     Set<Order> orders = new HashSet<>();
 
     public void addOrder(Order order) throws NullOrderException{
-        orders.add(order);
+        if (order == null) {
+            throw new NullOrderException();
+        } else {
+            orders.add(order);
+        }
     }
 
-    public List<Order> getOrdersFromGivenRangeOfDates(LocalDate min, LocalDate max) throws NoOrderInGivenRangeException {
-        LocalDate newMin = min.minusDays(1);
-        LocalDate newMax = max.plusDays(1);
-        return orders.stream().filter(u->u.getDate().isAfter(newMin)).filter(u->u.getDate().isBefore(newMax)).sorted(Comparator.comparing(Order::getDate)).collect(Collectors.toList());
+    public List<Order> getOrdersFromGivenRangeOfDates(LocalDate min, LocalDate max)
+            throws NoOrderInGivenRangeException {
+        List<Order> list = orders
+                .stream()
+                .filter(u->u.getDate().compareTo(min) >= 0)
+                .filter(u->u.getDate().compareTo(max) <= 0)
+                .sorted(Comparator.comparing(Order::getDate))
+                .collect(Collectors.toList());
+        if (list.size() == 0)
+            throw new NoOrderInGivenRangeException();
+        return list;
     }
 
-    public List<Order> getOrdersFromGivenRangeOfValues(double min, double max) throws NoOrderInGivenRangeException {
-        return orders.stream().filter(u->u.getPrice()>=min).filter(u->u.getPrice()<=max).sorted(Comparator.comparing(Order::getPrice)).collect(Collectors.toList());
+    public List<Order> getOrdersFromGivenRangeOfValues(double min, double max)
+            throws NoOrderInGivenRangeException {
+        List<Order> list = orders.stream()
+                .filter(u->u.getPrice()>=min)
+                .filter(u->u.getPrice()<=max)
+                .sorted(Comparator.comparing(Order::getPrice))
+                .collect(Collectors.toList());
+        if (list.size() == 0)
+            throw new NoOrderInGivenRangeException();
+        return list;
     }
 
     public int getNumbersOfOrders(){
--- kodilla-advanced-tests/src/test/java/com/kodilla/execution_model/homework/ShopTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/execution_model/homework/ShopTestSuite.java
@@ -1,11 +1,11 @@
 package com.kodilla.execution_model.homework;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.time.LocalDate;
 import java.util.*;
 import static org.junit.Assert.*;
-//import static org.junit.jupiter.api.Assertions.*;
 
 public class ShopTestSuite {
 
@@ -13,10 +13,14 @@ public class ShopTestSuite {
 
     @BeforeEach
     public void add_To_Orders() throws NullOrderException {
-        shop.addOrder(new Order("Order1", 100.99, LocalDate.of(2020, 01, 3), "abcd"));
-        shop.addOrder(new Order("Order2", 110.99, LocalDate.of(2020, 01, 4), "efgh"));
-        shop.addOrder(new Order("Order3", 120.99, LocalDate.of(2020, 01, 5), "ijkl"));
-        shop.addOrder(new Order("Order4", 130.99, LocalDate.of(2020, 01, 6), "mnop"));
+        shop.addOrder(new Order("Order1", 100.99, LocalDate.of(2020, 01, 3),
+                "abcd"));
+        shop.addOrder(new Order("Order2", 110.99, LocalDate.of(2020, 01, 4),
+                "efgh"));
+        shop.addOrder(new Order("Order3", 120.99, LocalDate.of(2020, 01, 5),
+                "ijkl"));
+        shop.addOrder(new Order("Order4", 130.99, LocalDate.of(2020, 01, 6),
+                "mnop"));
     }
 
     @Test
@@ -29,36 +33,33 @@ public class ShopTestSuite {
         shop.orders.clear();
         assertEquals(0, shop.getNumbersOfOrders());
     }
+
     @Test
-    public void Should_Return_Exception_When_Adding_Null_Order() throws NullOrderException {
+    public void Should_Return_Exception_When_Adding_Null_Order() {
         try {
             shop.addOrder(null);
         } catch (NullOrderException e){
             assertEquals("Create and order first.", e.getMessage());
         }
     }
-
-    //DODATKOWY TEST Z UŻYCIEM METODY NA KTÓRĄ POZWALA JUNIT4 (import static org.junit.Assert.*;)
-
-//    @org.junit.Test (expected = NullOrderException.class)
-//    public void Should_Return_Exception_When_Adding_Null_Order_Second_Method() throws NullOrderException {
-//        shop.addOrder(null);
-//    }
-    //DODATKOWY TEST Z UŻYCIEM METODY assertThrows - musiałem dodać bibliotekę import static org.junit.jupiter.api.Assertions.*;
-//    @Test
-//    public void Should_Return_Exception_When_Adding_Null_Order_Third_Method() throws NullOrderException {
-//        assertThrows(NullOrderException.class, () ->shop.addOrder(null), "Null Order Exception");
-//    }
+    @Test
+    public void Should_Return_Exception_When_Adding_Null_Order_Another_Method () {
+        Assertions.assertThrows(NullOrderException.class, () ->shop.addOrder(null));
+    }
 
     @Test
     public void Should_Get_Orders_From_Given_Range_Of_Dates() throws NoOrderInGivenRangeException {
         //GIVEN (FOR DATES BETWEEN 04.01.2020 and 06.01.2020)
         List<Order> expectedList = new ArrayList<>();
-        expectedList.add(new Order("Order2", 110.99, LocalDate.of(2020, 01, 4), "efgh"));
-        expectedList.add(new Order("Order3", 120.99, LocalDate.of(2020, 01, 5), "ijkl"));
-        expectedList.add(new Order("Order4", 130.99, LocalDate.of(2020, 01, 6), "mnop"));
+        expectedList.add(new Order("Order2", 110.99, LocalDate.of(2020, 01, 4),
+                "efgh"));
+        expectedList.add(new Order("Order3", 120.99, LocalDate.of(2020, 01, 5),
+                "ijkl"));
+        expectedList.add(new Order("Order4", 130.99, LocalDate.of(2020, 01, 6),
+                "mnop"));
         //WHEN (FOR DATES BETWEEN 04.01.2020 and 06.01.2020)
-        List<Order> actualList = shop.getOrdersFromGivenRangeOfDates(LocalDate.of(2020, 01, 4),LocalDate.of(2020, 01, 6));
+        List<Order> actualList = shop.getOrdersFromGivenRangeOfDates(LocalDate.of(2020, 01, 4),
+                LocalDate.of(2020, 01, 6));
         //THEN
         assertEquals(expectedList.get(0), actualList.get(0));
         assertEquals(expectedList.get(1), actualList.get(1));
@@ -66,28 +67,34 @@ public class ShopTestSuite {
     }
 
     @Test
-    public void Should_Return_Exception_When_There_Is_No_Order_In_Given_Range_Of_Dates() throws NoOrderInGivenRangeException {
+    public void Should_Return_Exception_When_There_Is_No_Order_In_Given_Range_Of_Dates() {
         try {
-            shop.getOrdersFromGivenRangeOfDates(LocalDate.of(2019, 01, 3), LocalDate.of(2019, 01, 6));
+            shop.getOrdersFromGivenRangeOfDates(LocalDate.of(2019, 01, 3),
+                    LocalDate.of(2019, 01, 6));
         } catch (NoOrderInGivenRangeException e) {
             assertEquals("There is no order in given range", e.getMessage());
         }
     }
 
-    //DODATKOWY TEST Z UŻYCIEM METODY NA KTÓRĄ POZWALA JUNIT4
+    @Test
+    public void Should_Return_Exception_When_There_Is_No_Order_In_Given_Range_Of_Dates_Anther_Method() {
+        Assertions.assertThrows(NoOrderInGivenRangeException.class, () ->
+                shop.getOrdersFromGivenRangeOfDates
+                        (LocalDate.of(2019, 01, 3),
+                                LocalDate.of(2019, 01, 6)));
+    }
 
-//    @org.junit.Test (expected = NoOrderInGivenRangeException.class)
-//    public void Should_Return_Exception_When_There_Is_No_Order_In_Given_Range_Of_Dates_Second_Method() throws NoOrderInGivenRangeException {
-//        shop.getOrdersFromGivenRangeOfDates(LocalDate.of(2019, 01, 3), LocalDate.of(2019, 01, 6));
-//    }
 
     @Test
     public void Should_Get_Orders_From_Given_Range_Of_Values() throws NoOrderInGivenRangeException {
         //GIVEN (FOR PRICES BETWEEN 100 and 130)
         List<Order> expectedList = new ArrayList<>();
-        expectedList.add(new Order("Order1", 100.99, LocalDate.of(2020, 01, 3), "abcd"));
-        expectedList.add(new Order("Order2", 110.99, LocalDate.of(2020, 01, 4), "efgh"));
-        expectedList.add(new Order("Order3", 120.99, LocalDate.of(2020, 01, 5), "ijkl"));
+        expectedList.add(new Order("Order1", 100.99, LocalDate.of(2020, 01, 3),
+                "abcd"));
+        expectedList.add(new Order("Order2", 110.99, LocalDate.of(2020, 01, 4),
+                "efgh"));
+        expectedList.add(new Order("Order3", 120.99, LocalDate.of(2020, 01, 5),
+                "ijkl"));
         //WHEN
         Order order1 = shop.getOrdersFromGivenRangeOfValues(100,130).get(0);
         Order order2 = shop.getOrdersFromGivenRangeOfValues(100,130).get(1);
@@ -99,12 +106,17 @@ public class ShopTestSuite {
     }
 
     @Test
-    public void Should_Return_Exception_When_There_Is_No_Order_In_Given_Range_Of_Values() throws NoOrderInGivenRangeException {
+    public void Should_Return_Exception_When_There_Is_No_Order_In_Given_Range_Of_Values() {
         try {
             shop.getOrdersFromGivenRangeOfValues(0, 50);
         } catch (NoOrderInGivenRangeException e) {
             assertEquals("There is no order in given range", e.getMessage());
         }
+    }
+
+    @Test
+    public void Should_Return_Exception_When_There_Is_No_Order_In_Given_Range_Of_Values_Another_Method() {
+        Assertions.assertThrows(NoOrderInGivenRangeException.class, ()-> shop.getOrdersFromGivenRangeOfValues(0,50));
     }
 
     @Test
